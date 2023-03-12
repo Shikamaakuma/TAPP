7e3b6723f6fa53f786f9d2ecd126e330469241d5
Kai Schoenenberger <kai.schoenenberger@bkosoft.ch>
2023-03-12 02:37:56 +0100

    #5 initial commit
    basic springboot webapp template

---
+++ src/main/java/tapp/TappApplication.java
@@ -0,0 +1,13 @@
+package tapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TappApplication.class, args);
+    }
+
+}
