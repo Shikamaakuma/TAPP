900164bf1aee6fb6848eee5f57b39428fb0db3c9
Markus Pfefferli <pfeffma1@students.zhaw.ch>
2023-05-02 23:04:00 +0200

    Added the tenantId to AthleteController (except for add athlete)

--- src/main/java/tapp/org/tapp/Controllers/AthleteController.java
+++ src/main/java/tapp/org/tapp/Controllers/AthleteController.java
@@ -3,64 +3,63 @@ package tapp.org.tapp.Controllers;
 
 import java.util.List;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import tapp.org.tapp.Repository.AthleteRepository;
 import tapp.org.tapp.Models.Athlete;
 
-import static org.springframework.data.jpa.domain.Specification.where;
-import static tapp.org.tapp.Repository.AthleteRepository.firstNameContains;
-import static tapp.org.tapp.Repository.AthleteRepository.lastNameContains;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class AthleteController {
 	@Autowired
 	private AthleteRepository athleteRepository;
+	@Autowired
+	EntityManager entityManager;
 
-	// get all athletes
-	@GetMapping("/athletes")
-	public List<Athlete> getAllAthletes(){
-		return athleteRepository.findAll();
-	}
-
-	@GetMapping("/allAthletes")
-	public String listAllAthletes(Model model)
-	{
-		athleteRepository.findAll();
-		return "AthletesOverview";
-	}
 
 	@PostMapping("/add_athlete")
 	public Athlete addAthlet(@RequestBody Athlete athlete) {
 		return athleteRepository.save(athlete);
 	}
 
-	@GetMapping("/listAthletes")
-	public String listAthlete(Model model) {
-		model.addAttribute("athleteList", getAllAthletes());
-
-		return "AthletesWhere";
-	}
+	/**
+	 * Returns all athletes for a given tenant ID
+	 * @param tenantId
+	 * @return list of athletes with tenantId
+	 */
+	@GetMapping("/{tenantId}/athletes")
+	public List<Athlete> allAthletesOfTenant(@PathVariable long tenantId) {return athleteRepository.findAthletesByTenant(tenantId);}
 
 	/**
-	 * Searches first and last name case-insensitive for the given input.
-	 * At the moment it does not work if given both a first and a last name!
-	 * @param searchinput
-	 * @return List of athletes which match the search criteria
+	 * Searches athletes with the first or last name containing the search term and the correct tenantId
+	 * @param tenantId
+	 * @param searchterm
+	 * @return List of athletes with tenantId and searchterm
 	 */
-	@GetMapping("/search_athlete/{searchinput}")
-	public List<Athlete> search(@PathVariable String searchinput) {return athleteRepository.findAll(where(firstNameContains(searchinput).or(lastNameContains(searchinput))));}
+	@GetMapping("/{tenantId}/athletes/{searchterm}")
+	public List<Athlete> findAthletesByName(@PathVariable long tenantId, @PathVariable String searchterm) {
 
-	@GetMapping("/search_athlete_firstname/{searchinput}")
-	public List<Athlete> searchFirstName(@PathVariable String searchinput) {return athleteRepository.findAll(where(firstNameContains(searchinput)));}
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Athlete> criteriaQuery = cb.createQuery(Athlete.class);
+		Root<Athlete> athlete = criteriaQuery.from(Athlete.class);
 
-	@GetMapping("/search_athlete_lastname/{searchinput}")
-	public List<Athlete> searchLastName(@PathVariable String searchinput) {return athleteRepository.findAll(where(lastNameContains(searchinput)));}
+		Predicate predicateFirstName = cb.like(cb.lower(athlete.get("firstName")), "%" + searchterm.toLowerCase().trim() + "%");
+		Predicate predicateLastName = cb.like(cb.lower(athlete.get("lastName")), "%" + searchterm.toLowerCase().trim() + "%");
+		Predicate predicateTenant = cb.equal(athlete.get("tenantId"), tenantId);
 
+		Predicate predicateName = cb.or(predicateLastName, predicateFirstName);
+		Predicate predicateTenantAndName = cb.and(predicateName, predicateTenant);
+		criteriaQuery.where(predicateTenantAndName);
+		List<Athlete> athletes = entityManager.createQuery(criteriaQuery).getResultList();
+		return athletes;
+	}
 }
 
--- src/main/java/tapp/org/tapp/Repository/AthleteRepository.java
+++ src/main/java/tapp/org/tapp/Repository/AthleteRepository.java
@@ -1,20 +1,17 @@
 package tapp.org.tapp.Repository;
 
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import tapp.org.tapp.Models.Athlete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface AthleteRepository extends JpaRepository<Athlete, Long>, JpaSpecificationExecutor<Athlete> {
-	static Specification<Athlete> firstNameContains(String searchinput) {
-		return (athlete, cq, cb) -> cb.like(cb.lower(athlete.get("firstName")), "%" + searchinput.toLowerCase().trim() + "%");
-	}
 
-	static Specification<Athlete> lastNameContains(String searchinput) {
-		return (athlete, cq, cb) -> cb.like(cb.lower(athlete.get("lastName")), "%" + searchinput.toLowerCase().trim() + "%");  //replaceAll("\\s","")
-	}
+	List<Athlete> findAthletesByName(long tenantId, String name);
+	List<Athlete> findAthletesByTenant(long tenantId);
 
 }
